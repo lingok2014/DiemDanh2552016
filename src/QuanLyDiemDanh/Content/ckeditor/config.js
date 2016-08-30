5005221cf3f121896e396d2cac40730e147a02e9
THANHTHINH\lingo <lingok2014@gmail.com>
2016-08-30 19:42:11 +0700

    comit

---
+++ src/QuanLyDiemDanh/Content/ckeditor/config.js
@@ -0,0 +1,28 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+    config.language = 'en';
+    // config.uiColor = '#AADC6E';
+    // cấu hình đường dẫn các loại tệp tin khi finder
+    config.filebrowserBrowseUrl = "/Content/ckfinder/ckfinder.html";
+    config.filebrowserImageUrl = "/Content/ckfinder/ckfinder.html?type=Images";
+    config.filebrowserFlashUrl = "/Content/ckfinder/ckfinder.html?type=Flash";
+    config.filebrowserUploadUrl = "/Content/ckfinder/core/connector/aspx/connector.aspx?command=Qui";
+    config.filebrowserImageUploadUrl = "/Content/ckfinder/core/connector/aspx/connector.aspx?command=Qui";
+    config.filebrowserFlashUploadUrl = "/Content/ckfinder/core/connector/aspx/connector.aspx?command=Qui";
+    config.filebrowserImageUploadUrl ="/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images";
+    config.filebrowserFlashUploadUrl ="/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash";
+
+
+
+
+
+};
+
+
+
+
---
+++ src/QuanLyDiemDanh/Controllers/GiaoViensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyDiemDanh.Models;
+
+namespace QuanLyDiemDanh.Controllers
+{
+    public class GiaoViensController : Controller
+    {
+        private DiemDanhSinhVienEntities db = new DiemDanhSinhVienEntities();
+
+        // GET: GiaoViens
+        public ActionResult Index()
+        {
+            return View(db.GiaoViens.ToList());
+        }
+
+        // GET: GiaoViens/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giaoVien);
+        }
+
+        // GET: GiaoViens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GiaoViens/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "gv_ma,gv_ten,gv_email,gv_sdt,gv_bangcap,gv_tendangnhap")] GiaoVien giaoVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GiaoViens.Add(giaoVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(giaoVien);
+        }
+
+        // GET: GiaoViens/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giaoVien);
+        }
+
+        // POST: GiaoViens/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "gv_ma,gv_ten,gv_email,gv_sdt,gv_bangcap,gv_tendangnhap")] GiaoVien giaoVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(giaoVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(giaoVien);
+        }
+
+        // GET: GiaoViens/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giaoVien);
+        }
+
+        // POST: GiaoViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            db.GiaoViens.Remove(giaoVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
